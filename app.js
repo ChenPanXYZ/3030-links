076a00b12bbb96d1c0210a542c8920bf83eeae4d
ChenPanXYZ <me@chenpan.xyz>
2021-06-01 17:42:58 -0400

    updated app.js

--- app.js
+++ app.js
@@ -27,15 +27,7 @@ app.use(session({
 	saveUninitialized: true
 }))
 
-app.use(express.static(path.join(__dirname, './client/build')))
-
-app.get('*', function(_, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
+app.use(express.static(__dirname + "/client/build"))
 
 // All routes other than above will go to index.html
 app.get("/Login|/Signup|Test", (req, res) => {
@@ -244,13 +236,13 @@ app.listen(port, () => {
 	log(`Listening on port ${port}...`)
 })
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+   if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
