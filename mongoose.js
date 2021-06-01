c9cf509311d7b7d48ba798127c11ee8572062980
ChenPanXYZ <me@chenpan.xyz>
2021-06-01 18:26:58 -0400

    deploy

--- mongoose.js
+++ mongoose.js
@@ -1,4 +1,4 @@
-const connectionString = process.env.MONGODB_URI
-const mongoose = require('mongoose')
-mongoose.connect(connectionString)
-module.exports = mongoose
+const connectionString = process.env.MONGODB_URI;
+const mongoose = require('mongoose');
+mongoose.connect(connectionString);
+module.exports = mongoose;
