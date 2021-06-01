57fa49139a40ed27fc26b41aa1df0a93a25bdcb6
ChenPanXYZ <me@chenpan.xyz>
2021-06-01 18:01:19 -0400

    use env.var for db password

--- mongoose.js
+++ mongoose.js
@@ -1,4 +1,4 @@
-const connectionString = process.env.MONGODB_URI
+const connectionString = 'mongodb+srv://chenpan:sD71fAu72N67Vm80@chenpan-8zw68.azure.mongodb.net/url-shortener?retryWrites=true&w=majority'
 const mongoose = require('mongoose')
 mongoose.connect(connectionString)
 
