dd14875eafc2ec9ddfb5e92eecfcfadc20e01250
ChenPanXYZ <me@chenpan.xyz>
2021-06-01 17:36:19 -0400

    updated app.js

--- app.js
+++ app.js
@@ -190,6 +190,12 @@ app.delete('/link', (req, res) => {
 })
 
 
+app.get('/hello', (req, res) => {
+    console.log("Someone says hello")
+    return res.send("3Q")
+})
+
+
 app.get('/:shortUrl([a-zA-Z0-9_-]+)', (req, res) => {
     const shorUrl = req.params.shortUrl
     Url.findOne(
@@ -229,3 +235,14 @@ const port = process.env.PORT || 43030
 app.listen(port, () => {
 	log(`Listening on port ${port}...`)
 })
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
